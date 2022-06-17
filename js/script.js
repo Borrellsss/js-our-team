e2aad99f22c675aca4dc979063c6669c67d2895c
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-06-17 17:53:55 +0200

    card added through js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,68 @@
 // Una volta definita la struttura dati, prendendo come riferimento
 // la card di esempio presente nell'html, stampare dinamicamente
 // una card per ogni membro del team.
+
+// *VARIABLES*
+// *arrays*
+// *dichiaro un array di "objects"*
+const teamMembers = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        photo: "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "img/scott-estrada-developer.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "img/walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "img/barbara-ramos-graphic-designer.jpg",
+    },
+];
+
+// *DOM elemets*
+const teamCardsWrapper = document.querySelector(".team-container");
+
+// *other global scope variables*
+let thisMember;
+
+// *LOGIC*
+for (let i = 0; i < teamMembers.length; i++) {
+    // *salvo nella variabile "thisMember" ogni singolo "object"*
+    // *presente nell'array "teamMembers"*
+    thisMember = teamMembers[i];
+
+    // !CONSOLE.LOG DI DEBUG!
+    // console.log(thisMember);
+
+    teamCardsWrapper.innerHTML +=`<div class="team-card">
+                                    <div class="card-image">
+                                    <img
+                                        src="${thisMember.photo}"
+                                        alt="Wayne Barnett"
+                                    />
+                                    </div>
+                                    <div class="card-text">
+                                    <h3>${thisMember.name}</h3>
+                                    <p>${thisMember.role}</p>
+                                    </div>
+                                </div>`;
+}
